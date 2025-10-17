fc70984c60829732207983792952b1b94349e02b
George Campbell <campbell.goe@gmail.com>
2025-10-17 20:35:49 +0100

    initial planet commit in r3f

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei'
+import PlanetMesh from "./planet";
+const SceneLighting = () => {
+  return <>
+    <ambientLight intensity={0.04} />
+    <directionalLight position={[0, 10, 0]} color="#ffff00" />
+    <directionalLight position={[-5, 10, -8]} color="#ff9900" />
+    <directionalLight position={[12, -10, -4]} color="green" />
+    <directionalLight position={[-15, -4, 12]} color="blue" />
+    
+    <directionalLight position={[-5, 12, -6]} color="red" />
+    {/* <directionalLight position={[0, 10, 0]} color="green" /> */}
+  </>
+}
+export default function App() {
+return (
+  <div className="w-full h-full">
+    <Canvas style={{width: '100%',height: '100dvh'}}>
+      <PlanetMesh wireframe={false} rotationSpeed={0.0001}/>
+      <SceneLighting />
+      <OrbitControls enablePan={false} />
+    </Canvas>
+    </div>
+);
+}
---
+++ src/components/planet.tsx
@@ -0,0 +1,23 @@
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
+import { useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
+
+export default function Planet({ size = 1.5, millisecondsPerLoop = 10000, detail = 1, wireframe = false, rotationSpeed = 0 }) {
+  const ref = useRef<THREE.Mesh>(null)
+  useFrame(() => {
+   const timeSize = (Math.cos(performance.now()/millisecondsPerLoop)/2+0.5)*size+size/2
+   if(ref.current){
+    const mesh = ref.current
+     if(rotationSpeed) (mesh.rotation.y = performance.now()*rotationSpeed);
+mesh.scale.set(timeSize, timeSize, timeSize)
+   } 
+  })
+return (
+      <mesh ref={ref}>
+        <dodecahedronGeometry args={[size, detail]} />
+        <meshStandardMaterial wireframe={wireframe} />
+      </mesh>
+);
+}
